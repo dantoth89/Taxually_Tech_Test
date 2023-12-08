1c2a0d240a03fde60da5b6161c12e3bede5bb4ac
Daniel Toth <dantoth89@gmail.com>
2023-12-08 10:12:52 +0100

    Add - VATRegistationRequest model

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VATRegistrationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Taxually.TechnicalTest.Models;
+
+public class VATRegistrationRequest
+{
+    public string CompanyName { get; set; }
+    public string CompanyId { get; set; }
+    public string Country { get; set; }
+}
