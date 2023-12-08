9b655886fb0eb705f83514c7df3c550e3f934db7
Daniel Toth <dantoth89@gmail.com>
2023-12-08 10:17:58 +0100

    Upd - Client update for testing

--- Taxually.TechnicalTest/Taxually.TechnicalTest/TaxuallyHttpClient.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/TaxuallyHttpClient.cs
@@ -2,7 +2,7 @@
 {
     public class TaxuallyHttpClient
     {
-        public Task PostAsync<TRequest>(string url, TRequest request)
+        public virtual Task PostAsync<TRequest>(string url, TRequest request)
         {
             // Actual HTTP call removed for purposes of this exercise
             return Task.CompletedTask;
--- Taxually.TechnicalTest/Taxually.TechnicalTest/TaxuallyQueueClient.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/TaxuallyQueueClient.cs
@@ -2,7 +2,7 @@
 {
     public class TaxuallyQueueClient
     {
-        public Task EnqueueAsync<TPayload>(string queueName, TPayload payload)
+        public virtual Task EnqueueAsync<TPayload>(string queueName, TPayload payload)
         {
             // Code to send to message queue removed for brevity
             return Task.CompletedTask;
