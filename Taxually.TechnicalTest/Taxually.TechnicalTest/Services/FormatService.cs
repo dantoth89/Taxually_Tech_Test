4782a3a304503eeb009d8805aa820a71bee9ecc8
Daniel Toth <dantoth89@gmail.com>
2023-12-08 10:13:51 +0100

    Add - FormatService for CSV and XML handling

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/FormatService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Services;
+
+public class FormatService : IFormatService
+{
+    public byte[] GenerateCsv(VATRegistrationRequest request)
+    {
+        var csvBuilder = new StringBuilder();
+        csvBuilder.AppendLine("CompanyName,CompanyId");
+        csvBuilder.AppendLine($"{request.CompanyName},{request.CompanyId}");
+        return Encoding.UTF8.GetBytes(csvBuilder.ToString());
+    }
+
+    public string GenerateXml(VATRegistrationRequest request)
+    {
+        using (var stringWriter = new StringWriter())
+        {
+            var serializer = new XmlSerializer(typeof(VATRegistrationRequest));
+            serializer.Serialize(stringWriter, request);
+            return stringWriter.ToString();
+        }
+    }
+}
---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/IFormatService.cs
@@ -0,0 +1,9 @@
+﻿using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Services;
+
+public interface IFormatService
+{
+    byte[] GenerateCsv(VATRegistrationRequest request);
+    string GenerateXml(VATRegistrationRequest request);
+}
